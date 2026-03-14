55e2fa8facee23c844c2ffe543c51ec8ffe1797a
Samuel Silva Gomes <silvagomes881@gmail.com>
2026-03-13 23:23:32 -0300

    fix cabou, add saida.txt
    - troquei cabo por cabou na análise léxica (top 10 mudanças de todos os tempos)
    
    - adicionei um arquivo de saida pro texto qua a main imprime no terminal

--- src/main.py
+++ src/main.py
@@ -17,12 +17,15 @@ def main() -> int:
     automato = construir_automato()
     lexer = Lexer(automato, mostrar_erros=True)
     lexer.carregar_arquivo(caminho_arquivo)
-    lexer.analisar()
-    tokens_identificados = [
-        token
-        for token in lexer.tokens
-        if token.tipo not in {TokenType.ERROR, TokenType.EOF}
-    ]
+    tokens_identificados = lexer.analisar()
+    
+    # bom tratamento mas deixei pra ver TODOS os tokens por enquanto
+    #tokens_identificados = [
+    #    token
+    #    for token in lexer.tokens
+    #    if token.tipo not in {TokenType.ERROR, TokenType.EOF}
+    #]
+    
     print(f"\nArquivo lido: {caminho_arquivo}")
     print(f"Tokens identificados: {len(tokens_identificados)}")
     print("\nTokens (tupla):")
@@ -31,7 +34,25 @@ def main() -> int:
     print("\nResumo:")
     print(f"- Identificados: {len(tokens_identificados)}")
     print(f"- Não identificados: {sum(1 for token in lexer.tokens if token.tipo == TokenType.ERROR)}")
+    
+    # Salvando em um arquivo .txt separado:
+    with open("exemplos/saida.txt", "w") as file:
+        file.write(f"Tokens identificados: {len(tokens_identificados)}\n")
+
+        file.write("\nTokens (tupla):\n")
+        for token in tokens_identificados:
+            file.write(str((token.lexema, token.tipo.name, token.linha, token.coluna)) + "\n")
+
+        file.write("\nResumo:\n")
+        file.write(f"- Identificados: {len(tokens_identificados)}\n")
+        file.write(
+        f"- Não identificados: {sum(1 for token in lexer.tokens if token.tipo == TokenType.ERROR)}\n"
+        )
+
+    
     return 0
+
+
 
 
 if __name__ == "__main__":
